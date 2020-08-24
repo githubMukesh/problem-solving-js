bc4281458e3cf177936db1e61441c81100b32b85
githubMukesh <getmkr@hotmail.com>
2020-08-25 00:12:30 +0530

    SumZero Impl done

--- multiplePointers/sumZero.js
+++ multiplePointers/sumZero.js
@@ -1,18 +1,18 @@
 //sumZero([-3,-2,-1,0,1,2,3]) // [-3,3]
 function sumZero(arr){
   const startPointer = 0;
-  const endPointer = arr.length;
+  const endPointer = arr.length - 1 ;
   for(let i=0; i < arr.length; i++){
      const sum = arr[startPointer] + arr[endPointer];
      if(sum === 0){
        return [arr[startPointer], arr[endPointer]];
      }
      else if(sum > 0){
-       arr[endPointer] -=  1;
+        endPointer -=  1;
      } else {
-       arr[startPointer] += 1;
+       startPointer += 1;
      }
   }
 }
 
-console.log(sumZero([-3,-2,-1,0,1,2,3]));
+console.log(sumZero([-3,-2,-1,0,1]));
